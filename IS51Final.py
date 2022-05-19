9da3ac694b258f33bb2d0123d3e2184a6840ca2a
Venancio Barela <calivee.ca@gmail.com>
2022-05-19 16:49:25 -0700

    finished structured-english

---
+++ IS51Final.py
@@ -0,0 +1,20 @@
+"""
+PLAIN ENGLISH
+set counter to 0
+open file in read
+capture variable (test scores) from list of grades (float)
+input variable
+calculate number of tests to be graded
+append grade scores to avg list
+calculate average grade for the class
+determine percent of grades that are above average
+(print) output grade information to user
+END
+"""
+
+"""
+
+
+
+
+"""
