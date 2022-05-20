034de03f82ecf689476d07f39833c399f006574d
Venancio Barela <calivee.ca@gmail.com>
2022-05-19 17:00:44 -0700

    finished english

--- IS51Final.py
+++ IS51Final.py
@@ -1,9 +1,9 @@
 """
 PLAIN ENGLISH
-set counter to 0
-open file in read
+get list of grades from txt.file (open in read mode (r))
+initalize counter to 0
 capture variable (test scores) from list of grades (float)
-input variable
+input grade
 calculate number of tests to be graded
 append grade scores to avg list
 calculate average grade for the class
@@ -13,7 +13,6 @@ END
 """
 
 """
-
 
 
 
